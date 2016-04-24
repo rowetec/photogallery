0738567764e63fa83e86f65687c13dad2a9bbb1b
Chris Rowe <chris@rowetec.co.uk>
2016-04-24 02:29:47 +0100

    Added ability to resize images

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 
 <head>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script> 
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	
 	<script src="scripts/photogallery.js"></script>
 	<link href="style/main.css" rel="stylesheet">
 	<title>Photo Gallery</title>
@@ -13,6 +17,12 @@
 		</div>
 		<div id="content">
 			<div id="filter">
+				<div id="thumbsize" class="btn-group btn-group-sm" role="group" aria-label="...">
+					<button type="button" class="btn btn-default">1</button>
+					<button type="button" class="btn btn-default active">2</button>
+					<button type="button" class="btn btn-default">3</button>
+					<button type="button" class="btn btn-default">4</button>
+				</div>
 			</div>
 			<div id="photopane">
 			</div>
--- scripts/photogallery.js
+++ scripts/photogallery.js
@@ -1,17 +1,30 @@
 jQuery(function(){
 	
-	maxImageWidth = 200;
-	maxImageHeight = 200;
+	defaultMaxImageWidth = 200;
+	defaultMaxImageHeight = 200;
+	maxImageWidth = defaultMaxImageWidth;
+	maxImageHeight = defaultMaxImageHeight;
 	imageMargin = 20;
 	photoPaneWidth = jQuery("#maingallerydiv #photopane").width;
 	galleryImages = [];
 	loadGallery();
 	
 	var resizeTimer;
+	
+	//Listener for windows size change
 	jQuery(window).on('resize',function(){
 		clearTimeout(resizeTimer);
 		resizeTimer = setTimeout(resizeGallery, 250, true);
 	});
+	//Listener for thumbnail size change
+	jQuery("#maingallerydiv #content #filter #thumbsize .btn").click(function(){
+		var btnVal = jQuery(this).html();
+		maxImageWidth = defaultMaxImageWidth * (btnVal - 0.75 - (0.25 * (btnVal - 1)));
+		maxImageHeight = defaultMaxImageHeight * (btnVal - 0.75 - (0.25 * (btnVal - 1)));
+		jQuery(this).addClass("active").siblings().removeClass("active");
+		resizeGallery(true,true);
+	});
+	
 });
 
 loadGallery = function(){
@@ -22,6 +35,7 @@ loadGallery = function(){
 		var html = "";//"<div id=\"spacer\"></div>";
 		var i = 0;
 		albumImages.forEach(function(item){
+			var id = "thumb_"+i;
 			if (item.width > item.height){
 				var orientation = "landscape";
 				var dimensionRatio = item.height / item.width;
@@ -34,11 +48,11 @@ loadGallery = function(){
 				var thumbHeight = 200;
 				var thumbWidth = thumbHeight * dimensionRatio;
 			}
-			galleryImages[i] = new galleryImage (item.filename, item.width, item.height, orientation, dimensionRatio, thumbHeight, thumbWidth);
+			galleryImages[i] = new galleryImage (item.filename, item.width, item.height, orientation, dimensionRatio, thumbHeight, thumbWidth,id);
 			i++;
 		});
 		galleryImages.forEach(function(item){
-			html = html + '<div class="albumimagecontainer"><div class="albumimagehorizonalalign"><div class="albumimage"><img src="photos/thumbs/'+item.filePath+'" height="'+item.thumbHeight+'" width="'+item.thumbWidth+'" /></div></div></div>';
+			html = html + '<div id="'+item.id+'" class="albumimagecontainer"><div class="albumimagehorizontalalign"><div class="albumimage"><img src="photos/thumbs/'+item.filePath+'" height="'+item.thumbHeight+'" width="'+item.thumbWidth+'" /></div></div></div>';
 		});
 		
 		jQuery("#maingallerydiv #photopane").html(html);
@@ -54,16 +68,16 @@ loadGallery = function(){
 function showImages(object) {
 	//show, find next image, repeat, when next doesn't find anything, it'll stop
 	object.width(maxImageWidth+"px").height(maxImageHeight+"px")
-	.children(".albumimagehorizonalalign").width(object.children(".albumimagehorizonalalign").children(".albumimage").children("img").width())
-	.children(".albumimage").width(object.children(".albumimagehorizonalalign").children(".albumimage").children("img").width()).height(object.children(".albumimage")
+	.children(".albumimagehorizontalalign").width(object.children(".albumimagehorizontalalign").children(".albumimage").children("img").width())
+	.children(".albumimage").width(object.children(".albumimagehorizontalalign").children(".albumimage").children("img").width()).height(object.children(".albumimage")
 	.children("img").height()).show()
 	.children("img").fadeIn(100);
 	setTimeout(function(){
 		showImages(jQuery(object).next());
 	},10);
-};
+}
 
-function galleryImage (filePath,width,height,orientation,dimensionRatio,thumbHeight,thumbWidth){
+function galleryImage (filePath,width,height,orientation,dimensionRatio,thumbHeight,thumbWidth,id){
 	this.filePath 		= filePath;
 	//this.galleryId 	= galleryId;
 	this.width	 		= width;
@@ -73,18 +87,23 @@ function galleryImage (filePath,width,height,orientation,dimensionRatio,thumbHei
 	this.thumbHeight	= thumbHeight;
 	this.thumbWidth		= thumbWidth;
 	//this.dateTime		= dateTime;
-	//this.Id			= Id;
+	this.id				= id;
 }
 
-function resizeGallery(animate) {
+function resizeGallery(animate,resizeThumbs) {
 	//jQuery(window).off("resize");
 	var numberOfImagesAcross = Math.floor((jQuery("#maingallerydiv #photopane").width()-15)/(imageMargin*2+maxImageWidth));
 	
 	
-	var spacerWidth = Math.floor(((jQuery("#maingallerydiv #photopane").width() - numberOfImagesAcross * (imageMargin*2+maxImageWidth))) / 2);
+	var spacerWidth = Math.floor((jQuery("#maingallerydiv #photopane").width() - numberOfImagesAcross * (imageMargin*2+maxImageWidth)) / 2 + imageMargin/2);
 	var i = 0;
 	var column = 1;
 	var row = 1;
+	
+	if (resizeThumbs){
+		resizeThumbnails();
+	}
+	
 	jQuery("#maingallerydiv .albumimagecontainer").each(function(){
 		
 		if ((Math.floor(i / numberOfImagesAcross)+1) != row){
@@ -104,37 +123,55 @@ function resizeGallery(animate) {
 		else{
 			fromLeft = (imageMargin*2+maxImageWidth)*(column-1)+imageMargin+spacerWidth;
 		}
-		if (!animate){
-			jQuery(this).stop().css({
-				"left" : fromLeft + "px",
-				"top" : fromTop + "px"
-			})
+		if (resizeThumbs){
+			jQuery(this).stop().animate({
+					"left" : fromLeft + "px",
+					"top" : fromTop + "px",
+					"width" : maxImageWidth + "px",
+					"height" : maxImageWidth + "px"
+				})
 		}
 		else {
-			jQuery(this).stop().animate({
-				"left" : fromLeft + "px",
-				"top" : fromTop + "px"
-			})
+			if (!animate){
+				jQuery(this).stop().css({
+					"left" : fromLeft + "px",
+					"top" : fromTop + "px"
+				})
+			}
+			else {
+				jQuery(this).stop().animate({
+					"left" : fromLeft + "px",
+					"top" : fromTop + "px"
+				})
+			}
 		}
 		i++;
 		column++;
 	});
-	/*setTimeout(function(){
-		jQuery(window).resize(function(){
-			resizeGallery();
-		});
-	},1000);
-	*/
-	//jQuery("#maingallerydiv #photopane #spacer").width(spacerWidth+"px");
-};
 
-/*function resizeGallery() {
-	var numberOfImageAcross = Math.floor((jQuery("#maingallerydiv #photopane").width()-15)/(imageMargin*2+maxImageWidth));
-	console.log ("Div Width: "+jQuery("#maingallerydiv #photopane").width());
-	console.log ("No Of Images: "+numberOfImageAcross);
-	console.log ("Margin: "+imageMargin);
-	console.log ("Image Width: "+maxImageWidth);
-	
-	var spacerWidth = Math.floor(((jQuery("#maingallerydiv #photopane").width() - numberOfImageAcross * (imageMargin*2+maxImageWidth))) / 2);
-	jQuery("#maingallerydiv #photopane #spacer").width(spacerWidth+"px");
-};*/
+}
+
+function resizeThumbnails(){
+	galleryImages.forEach(function(item){
+		if (item.orientation == "landscape"){
+			item.thumbWidth = maxImageWidth;
+			item.thumbHeight = maxImageWidth * item.dimensionRatio;
+		}
+		else{
+			item.thumbHeight = maxImageHeight;
+			item.thumbWidth = maxImageHeight * item.dimensionRatio;
+		}
+		jQuery("#" + item.id + " img").stop().animate({
+			"width" : item.thumbWidth + "px",
+			"height" : item.thumbHeight + "px"
+		});
+		jQuery("#" + item.id + " .albumimagehorizontalalign").stop().animate({
+			"width" : item.thumbWidth + "px"
+		});
+		jQuery("#" + item.id + " .albumimage").stop().animate({
+			"width" : item.thumbWidth + "px",
+			"height" : item.thumbHeight + "px"
+		});
+	})
+}
+
